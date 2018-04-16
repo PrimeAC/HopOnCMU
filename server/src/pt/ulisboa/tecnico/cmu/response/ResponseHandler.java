444b952ec68d1785d79eb6efdfd48c4414ab090b
brunoaosantos <bruno_scp5@hotmail.com>
2018-04-16 14:43:17 +0100

    fix indentation

--- server/src/pt/ulisboa/tecnico/cmu/response/ResponseHandler.java
+++ server/src/pt/ulisboa/tecnico/cmu/response/ResponseHandler.java
@@ -6,8 +6,8 @@ import pt.ulisboa.tecnico.cmu.command.SignUpCommand;
 public interface ResponseHandler {
 	public void handle(HelloResponse hr);
 	public void handle(TicketResponse tr);
-  public void handle(GetQuizResponse gqr);
+  	public void handle(GetQuizResponse gqr);
 	public void handle(SignUpResponse sur);
-  public void handle(GetMonumentsResponse gmr);
+  	public void handle(GetMonumentsResponse gmr);
   	public void handle(GetRankingResponse grr);
 }
