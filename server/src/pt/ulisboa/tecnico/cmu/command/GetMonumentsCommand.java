c2b517373e765cd71018375b383a21bf1eb8f76f
brunoaosantos <bruno_scp5@hotmail.com>
2018-04-18 19:57:54 +0100

    remove string from GetMonumentsCommand

--- server/src/pt/ulisboa/tecnico/cmu/command/GetMonumentsCommand.java
+++ server/src/pt/ulisboa/tecnico/cmu/command/GetMonumentsCommand.java
@@ -5,19 +5,14 @@ import pt.ulisboa.tecnico.cmu.response.Response;
 public class GetMonumentsCommand implements Command {
 
     private static final long serialVersionUID = -8807331723807741905L;
-    private String message;
 
-    public GetMonumentsCommand(String message) {
-        this.message = message;
+    public GetMonumentsCommand() {
+
     }
 
     @Override
     public Response handle(CommandHandler chi) {
         return chi.handle(this);
-    }
-
-    public String getMessage() {
-        return this.message;
     }
 
 }
