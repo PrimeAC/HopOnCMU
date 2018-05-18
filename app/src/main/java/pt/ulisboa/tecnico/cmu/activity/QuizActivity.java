aef563a87a2b56019819444626a4e9fa7f15a518
PrimeAC <afonso.caetano@outlook.com>
2018-05-18 23:44:06 +0100

    First submission commit

--- app/src/main/java/pt/ulisboa/tecnico/cmu/activity/QuizActivity.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmu/activity/QuizActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
@@ -13,7 +14,6 @@ import android.widget.Toast;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
 
